7e6aceaaad6ed3e45d1d6c2954fac4f80c5a0c9c
Maline30 <malin.subaru@gmail.com>
2019-03-13 14:59:02 +0100

    Calculator Test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,55 @@
 package com.kodilla.testing;
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("First test Calculator");
+
+        Calculator calculator = new Calculator(1,1);
+
+        int addTest = calculator.add();
+
+        if(addTest == 2){
+            System.out.println("Add Test OK");
+        }else{
+            System.out.println("Error");
+        }
+
+        int subtractTest = calculator.subtract();
+
+        if(subtractTest == 0){
+            System.out.println("Subtract Test OK");
+        }else{
+            System.out.println("Error");
+        }
+
+        int testA = calculator.getA();
+
+        if(testA == 1){
+            System.out.println("Write Number A Test OK");
+        }else{
+            System.out.println("Error");
+        }
+
+        int testB = calculator.getB();
+
+        if(testB == 1){
+            System.out.println("Write Number B Test OK");
+        }else{
+            System.out.println("Error");
+        }
+
+
     }
 }
