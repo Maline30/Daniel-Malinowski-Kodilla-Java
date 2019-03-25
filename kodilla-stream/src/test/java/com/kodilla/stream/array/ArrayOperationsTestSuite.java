a7567447d75b06c1a805b509605252bb39c43975
Maline30 <malin.subaru@gmail.com>
2019-03-25 14:23:14 +0100

    task:Calculate the average, functionally

---
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -0,0 +1,19 @@
+package com.kodilla.stream.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ArrayOperationsTestSuite {
+
+        @Test
+        public void testGetAverage(){
+            //Given
+            int numbers [] = {2,4,5,7,8,9,3,4,3,2,6,8,4};
+            double averageExpected = 5.0;
+            //When
+            double average = ArrayOperations.getAverage(numbers);
+            //Then
+            Assert.assertEquals(averageExpected, average, 0.0001);
+        }
+}
+
